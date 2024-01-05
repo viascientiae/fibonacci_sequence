9a99bb952939bbb3dcd718cd6a50798b49b6df52
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2024-01-05 14:23:39 +0530

    Define #fibs_rec using recursion
     - Solves the same problem as the previously committed #fibs, but by
     using recursion instead of iteration

--- fibonacci_sequence.rb
+++ fibonacci_sequence.rb
@@ -18,3 +18,11 @@ def fibs(n)
 
   return sequence
 end
+
+def fibs_rec(n)
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  sequence = fibs_rec(n - 1)
+  sequence << sequence[-1] + sequence[-2]
+end
